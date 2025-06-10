e8e59d9124b3b42196e8992fbfb9480cb93a44df
adambigg-s <biggsa601@gmail.com>
2025-06-10 14:45:55 -0400

    matrix 2

--- src/matrix.zig
+++ src/matrix.zig
@@ -2,6 +2,68 @@ const lib = @import("root.zig");
 const std = lib.std;
 const vec = lib.vec;
 
+pub fn Mat2(comptime T: type) type {
+    return struct {
+        inner: [Self.dim][Self.dim]T,
+
+        pub const dim = 2;
+
+        const Self = @This();
+
+        pub fn identity() Self {
+            comptime var inner: [Self.dim][Self.dim]T = undefined;
+            comptime {
+                for (0..Self.dim) |i| {
+                    for (0..Self.dim) |j| {
+                        inner[i][j] = if (i == j) 1 else 0;
+                    }
+                }
+            }
+            return Self{ .inner = inner };
+        }
+
+        pub fn zeros() Self {
+            comptime var inner: [Self.dim][Self.dim]T = undefined;
+            comptime {
+                for (0..Self.dim) |i| {
+                    for (0..Self.dim) |j| {
+                        inner[i][j] = 0;
+                    }
+                }
+            }
+
+            return Self{ .inner = inner };
+        }
+
+        pub fn mulVec(self: *const Self, vector: vec.Vec2(T)) vec.Vec2(T) {
+            const m = self.inner;
+            const v = vector;
+
+            return vec.Vec3(T).build(
+                m[0][0] * v.x + m[0][1] * v.y,
+                m[1][0] * v.x + m[1][1] * v.y,
+            );
+        }
+
+        pub fn mulMat(self: *const Self, other: *const Self) Self {
+            const a = self.inner;
+            const b = other.inner;
+
+            var inner: [Self.dim][Self.dim]T = Self.zeros().inner;
+
+            for (0..Self.dim) |i| {
+                for (0..Self.dim) |j| {
+                    for (0..Self.dim) |k| {
+                        inner[i][j] += a[i][k] * b[k][j];
+                    }
+                }
+            }
+
+            return Self{ .inner = inner };
+        }
+    };
+}
+
 pub fn Mat3(comptime T: type) type {
     return struct {
         inner: [Self.dim][Self.dim]T,
